95457a2b1fa58dfa19f1aec8622895307ca85212
icraft2170 <icraft2170@gmail.com>
2022-04-09 01:13:33 +0900

    UserDao 기본 형태

---
+++ src/main/java/me/hero/user/dao/UserDao.java
@@ -0,0 +1,63 @@
+package me.hero.user.dao;
+
+import me.hero.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement("select  * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/toby", "toby", "see3470");
+    }
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User findUser = dao.get(user.getId());
+        System.out.println(findUser.getName());
+    }
+}
