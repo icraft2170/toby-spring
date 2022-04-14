73adc709ba38ac1ab35c50bb33866ecbe33cd57b
icraft2170 <icraft2170@gmail.com>
2022-04-14 23:09:49 +0900

    2장 : getCount() 테스트 추가

--- src/test/java/me/hero/user/dao/UserDaoTest.java
+++ src/test/java/me/hero/user/dao/UserDaoTest.java
@@ -34,4 +34,26 @@ class UserDaoTest {
         assertEquals(user.getPassword(), findUser.getPassword());
     }
 
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumme", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertEquals(0, dao.getCount());
+
+        dao.add(user1);
+        assertEquals(1, dao.getCount());
+
+        dao.add(user2);
+        assertEquals(2, dao.getCount());
+
+        dao.add(user3);
+        assertEquals(3, dao.getCount());
+
+    }
 }
