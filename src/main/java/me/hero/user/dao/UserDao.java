e6ba187b3cdddef7446eb78c384669e7364680b4
icraft2170 <icraft2170@gmail.com>
2022-04-18 22:30:50 +0900

    3장 : 변하지 않는 템플릿을 메소드로 분리

--- src/main/java/me/hero/user/dao/UserDao.java
+++ src/main/java/me/hero/user/dao/UserDao.java
@@ -8,6 +8,7 @@ import java.sql.*;
 
 public class UserDao {
     private final DataSource dataSource;
+
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
@@ -74,24 +75,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        }finally {
-            if (ps != null) {
-                try { ps.close(); } catch (SQLException e){}
-            }
-            if (c != null) {
-                try { c.close(); } catch (SQLException e){}
-            }
-        }
+        StatementStrategy stmt = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(stmt);
     }
 
     public int getCount() throws SQLException {
@@ -112,6 +97,26 @@ public class UserDao {
             if (rs != null) {
                 try { rs.close(); } catch (SQLException e){}
             }
+            if (ps != null) {
+                try { ps.close(); } catch (SQLException e){}
+            }
+            if (c != null) {
+                try { c.close(); } catch (SQLException e){}
+            }
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }finally {
             if (ps != null) {
                 try { ps.close(); } catch (SQLException e){}
             }
