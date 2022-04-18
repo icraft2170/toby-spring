56712ca458e7bbe77c6ed972fcf43065595ca480
icraft2170 <icraft2170@gmail.com>
2022-04-18 21:46:53 +0900

    3장 : Dao에 예외처리 - UserDao에 모든 메소드에 대한 try/catch/finally 추가하기

--- src/main/java/me/hero/user/dao/UserDao.java
+++ src/main/java/me/hero/user/dao/UserDao.java
@@ -12,42 +12,55 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException, ClassNotFoundException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+    public void add(User user) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        ps.executeUpdate();
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        ps.close();
-        c.close();
+            ps.executeUpdate();
+        } catch (SQLException e) {
+         throw e;
+        } finally {
+            ps.close();
+            c.close();
+        }
     }
 
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select  * from users where id = ?");
-        ps.setString(1, id);
+    public User get(String id) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        ResultSet rs = ps.executeQuery();
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select  * from users where id = ?");
+            ps.setString(1, id);
 
-        User user = null;
-        if(rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
+            rs = ps.executeQuery();
+
+            User user = null;
+            if(rs.next()) {
+                user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+            if (user == null) throw new EmptyResultDataAccessException(1);
+            return user;
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            rs.close();
+            ps.close();
+            c.close();
         }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if (user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
     }
 
     public void deleteAll() throws SQLException {
@@ -67,16 +80,23 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = dataSource.getConnection();
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+            ps = c.prepareStatement("select count(*) from users");
 
-        rs.close();
-        ps.close();
-        c.close();
-        return count;
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            rs.close();
+            ps.close();
+            c.close();
+        }
     }
 }
